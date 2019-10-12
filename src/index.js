5ca179bedd1aa72faca46040d4d145cfd20b618a
Anton <samusenkoanton@yandex.ru>
2019-10-13 02:31:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
     str = str.split('');;
-    console.log(str);
     for (let i = 0; i < str.length; i++) {
         for (let j = 0; j < bracketsConfig.length; j++) {
             let firstElement = str[i] === bracketsConfig[j][0];
-            console.log("Проверка"+str[i] + str[i+1]);
             let lastElement = str[i + 1] === bracketsConfig[j][1];
             if (firstElement && lastElement) {
                 str.splice(i, 2);
